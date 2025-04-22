3ecab6d95ba4b3d4a185f1431b5175d284b61d7e
Akeur <arkream41@gmail.com>
2025-04-23 01:27:49 +0300

    Docx filling programm

---
+++ Docs/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Docs.Views;
+
+namespace Docs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowView MWV { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            MWV = new MainWindowView();
+
+        }
+
+        private void Button_CalculateFile(object sender, RoutedEventArgs e)
+        {
+            MWV.CalculateDoc();
+        }
+    }
+}
---
+++ Docs/Views/MainWindowView.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Docs.Views
+{
+    public class MainWindowView : BaseView
+    {
+        private List<string> list = new List<string>{ "{NAME}", "{COURSE}", "{FIELD_OF_STUDY}", "{GROUP}", "{DISCIPLINE}", "{THEME}", "{TEACHER}", "{DATE_YEAR}", "{DATE_MONTH}", "{DATE_DAY}" };
+        private string name = "x";
+        private string course = "3";
+        private string fieldOfStudy = "Информатика и вычислительная техника";
+        private string group = "x";
+        private string discipline = "Функциональное и логическое программирование";
+        private string theme = "x";
+        private string teacher = "x";
+        WordprocessingDocument doc = WordprocessingDocument.Open(@"C:\Github\Docs\test.docx", true);
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; OnPropertyChanged(nameof(Name)); }
+        }
+        public string Course
+        {
+            get { return course; }
+            set { course = value; OnPropertyChanged(nameof(Course)); }
+        }
+        public string FieldOfStudy
+        {
+            get { return fieldOfStudy; }
+            set { fieldOfStudy = value; OnPropertyChanged(nameof(FieldOfStudy)); }
+        }
+        public string Group
+        {
+            get { return group; }
+            set { group = value; OnPropertyChanged(nameof(Group)); }
+        }
+        public string Discipline
+        {
+            get { return discipline; }
+            set { discipline = value; OnPropertyChanged(nameof(Discipline)); }
+        }
+        public string Theme
+        {
+            get { return theme; }
+            set { theme = value; OnPropertyChanged(nameof(Theme)); }
+        }
+        public string Teacher
+        {
+            get { return teacher; }
+            set { teacher = value; OnPropertyChanged(nameof(Teacher)); }
+        }
+
+        public void CalculateDoc()
+        {
+            var body = doc.MainDocumentPart.Document.Body;
+            var paras = body.Elements<Paragraph>();
+            string? docText = null;
+            int i = 0;
+            string text = null;
+            using (var savedDoc = doc.Clone(@"C:/Github/Docs/result.docx"))
+            {
+                foreach (var line in list)
+                {
+                    switch (line)
+                    {
+                        case "{NAME}":
+                            text = Name;
+                            break;
+                        case "{COURSE}":
+                            text = Course;
+                            break;
+                        case "{FIELD_OF_STUDY}":
+                            text = FieldOfStudy;
+                            break;
+                        case "{GROUP}":
+                            text = Group;
+                            break;
+                        case "{DISCIPLINE}":
+                            text = Discipline;
+                            break;
+                        case "{THEME}":
+                            text = Theme;
+                            break;
+                        case "{TEACHER}":
+                            text = Teacher;
+                            break;
+                        case "{DATE_YEAR}":
+                            text = DateTime.Now.Year.ToString();
+                            break;
+                        case "{DATE_MONTH}":
+                            text = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(DateTime.Now.Month);
+                            break;
+                        case "{DATE_DAY}":
+                            text = DateTime.Now.Day.ToString();
+                            break;
+                        default:
+                            break;
+                    }
+                    if (savedDoc.MainDocumentPart is null)
+                    {
+                        throw new ArgumentNullException("MainDocumentPart and/or Body is null.");
+                    }
+
+                    using (StreamReader sr = new StreamReader(savedDoc.MainDocumentPart.GetStream()))
+                    {
+                        docText = sr.ReadToEnd();
+                    }
+
+                    Regex regexText = new Regex(line);
+                    docText = regexText.Replace(docText, text);
+
+                    using (StreamWriter sw = new StreamWriter(savedDoc.MainDocumentPart.GetStream(FileMode.Create)))
+                    {
+                        sw.Write(docText);
+                    }
+                }
+            }
+        }
+    
+    }
+
+}
